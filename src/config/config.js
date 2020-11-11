c16cd4fbffa0f569bd3ad89f44ca23b0cc8b5938
Ashwini Kasbekar <kasbekarash93@gmail.com>
2020-11-11 15:21:12 -0800

    use remote mongo

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,17 @@ const config = {
     production: {
       secret: process.env.secret,
       MONGO_URI: process.env.MONGO_URI,
-      port: process.env.PORT,
+      //port: process.env.PORT,
     },
+    // development: {
+    //   secret: 'AASSDDAA',
+    //   MONGO_URI: 'mongodb://localhost:27017/mydb',
+    //   port: 3000,
+    // },
     development: {
       secret: 'AASSDDAA',
-      MONGO_URI: 'mongodb://localhost:27017/mydb',
-      port: 3000,
+      MONGO_URI: 'mongodb+srv://mydb:ashwini@cluster0.xoh8t.mongodb.net/test?retryWrites=true&w=majority',
+      //port: 3000,
     },
   };
   
